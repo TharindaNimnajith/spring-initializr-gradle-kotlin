0fc5bff06b920216bdd9766ffb7064d8a0fc336c
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-08 02:37:05 +0530

    init

---
+++ src/main/kotlin/com/example/springinitializrgradlekotlin/SpringInitializrGradleKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springinitializrgradlekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringInitializrGradleKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringInitializrGradleKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springinitializrgradlekotlin/SpringInitializrGradleKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springinitializrgradlekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringInitializrGradleKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
